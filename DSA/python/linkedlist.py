ba0cee3500f0a671ac84316b732b58b01e2878ab
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-02-18 10:31:02 +0545

    Iteration to linkedlist

--- DSA/python/linkedlist.py
+++ DSA/python/linkedlist.py
@@ -3,11 +3,16 @@ class Node:
         self.value = value
         self.next = next
 
+    def __str__(self):
+        return str(self.value)
+
 
 class LinkedList:
     def __init__(self, headValue=None):
         self.head = Node(headValue)
         self.tail = self.head
+        self.index = 0
+        self.iter_node = self.head
 
     def __str__(self):
         s = "LinkedList ("
@@ -23,6 +28,18 @@ class LinkedList:
         s += ")"
         return s
 
+    def __iter__(self):
+        self.index = 0
+        self.iter_node = self.head
+        return self
+
+    def __next__(self):
+        if self.iter_node is None:
+            raise StopIteration
+        return_value = self.iter_node.value
+        self.iter_node = self.iter_node.next
+        return return_value
+
     def append(self, value, next=None):
         self.tail.next = Node(value, next)
         self.tail = self.tail.next
@@ -34,11 +51,15 @@ class LinkedList:
         curr = self.head
         while curr.next.next is not None:
             curr = curr.next
+        return_value = curr.next.value
         curr.next = None
+        return return_value
 
     def pop_head(self):
         if self.head:
+            return_value = self.head.value
             self.head = self.head.next
+            return return_value
 
     def clear(self):
         self.head = None
@@ -49,10 +70,13 @@ if __name__ == "__main__":
     a.append(20)
     a.append(40)
     a.prepend("Houli")
+    print()
+    print("Iterating:")
+    for i in a:
+        print(i)
+    print()
     print("After prepend: ", a)
-    a.pop()
-    print("After pop:", a)
-    a.pop_head()
-    print("Pop head:", a)
+    print("After pop:", a.pop(), a)
+    print("Pop head:", a.pop_head(), a)
     a.clear()
     print("After clear:", a)
